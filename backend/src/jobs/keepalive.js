7b697e3c1b3357fc16ff2ad445cbe4d66860cef4
Prateek <prateekprajapati062004@gmail.com>
2026-08-23 17:51:49 +0530

    keep alive logs added

--- backend/src/jobs/keepalive.js
+++ backend/src/jobs/keepalive.js
@@ -1,6 +1,10 @@
+const logger = require('./../utils/logger.js');
+
 const ping = async () => {
 	try {
 		await fetch(process.env.keepAlive);
+		logger.info("Pinged the server to keep it alive.");
+
 	}
 	catch (error) {
 		logger.error("Error in ping", error);
